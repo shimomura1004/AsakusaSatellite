da7ccafc2fc495220110a0fa28d8dab2c665bbb1
SHIMOMURA Sho <graueule@gmail.com>
2013-09-07 23:41:26 +0900

    refactor desktop notification plugin refs #140

--- plugins/as_desktopnotification/lib/desktopnotification.rb
+++ plugins/as_desktopnotification/lib/desktopnotification.rb
@@ -1,32 +1,9 @@
 class DesktopnotificationListener < AsakusaSatellite::Hook::Listener
-  def read_js(filename)
-    js_path = Rails.root.join 'plugins/as_desktopnotification/app/assets/javascripts'
-    File.read(js_path.join filename)
-  end
+  js_path = plugin_root + "app/assets/javascripts/"
 
-  render_on  :account_setting_item, :partial => "desktopnotification_setting"
-
-  def global_footer(context)
-    controller = context[:request][:controller]
-    action     = context[:request][:action]
-
-    case {:controller => controller, :action => action}
-    when {:controller => "account", :action => "index"}
-      <<-JS
-      <script>#{read_js 'desktopnotification.js'}</script>
-      <script>#{read_js 'desktopnotification_setting.js'}</script>
-      JS
-    when {:controller => "chat",    :action => "room"}
-      <<-JS
-      <script>#{read_js 'desktopnotification.js'}</script>
-      <script>#{read_js 'desktopnotification_notify.js'}</script>
-      JS
-    end
-  end
-
-  private
-  def render(context, options)
-    context[:controller].send(:render_to_string, {:locals => context}.merge(options))
-  end
+  render_on :account_setting_item, :partial => "desktopnotification_setting"
+  render_on :script_in_account_setting, :jsfile => js_path + "desktopnotification.js"
+  render_on :script_in_account_setting, :jsfile => js_path + "desktopnotification_setting.js"
+  render_on :script_in_chat_room, :jsfile => js_path + "desktopnotification.js"
+  render_on :script_in_chat_room, :jsfile => js_path + "desktopnotification_notify.js"
 end
-
